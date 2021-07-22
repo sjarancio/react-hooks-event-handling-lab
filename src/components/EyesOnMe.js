3e56199ce23034670782ee98f7917234963197c6
Stephen Arancio <sjarancio@gmail.com>
2021-07-22 18:19:25 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function OnFocus(){
+        console.log('Good!')
+    }
+
+    function OnBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+    <button onFocus={OnFocus} onBlur={OnBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
